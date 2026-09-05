973f6d0faad1b44c7a5a152d97637c1bbec7c157
PrestonJawa <289257190+PrestonJawa@users.noreply.github.com>
2026-09-04 22:08:00 -0400

    Setup initial file structure with empty routers, controllers, and ejs

---
+++ route/instrumentRouter.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const instrumentController = require("../controller/instrumentController");
+
+const instrumentRouter = Router();
+
+module.exports = instrumentRouter;
